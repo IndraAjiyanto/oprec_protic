3c2203039925f681ced7e7f602adbb04566bf11a
IndraAjiyanto <indraajiyanto052@gmail.com>
2024-09-05 04:47:25 +0700

    update form

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["**/*.{html,js}"],
   theme: {
     extend: {
+      backgroundImage : {
+        'gojo' : "url('/asset/gojo.gif')",
+      },
       width: {
         '128': '32rem',
       },
